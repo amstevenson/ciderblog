db51630c5a6309dc65baaa5ae87f0e518598b447
amstevenson <addstevenson@hotmail.com>
2018-06-18 19:09:39 +0100

    added get image path for cover that determines extension automatically

--- src/main/java/ciderblog/cider/Cider.java
+++ src/main/java/ciderblog/cider/Cider.java
@@ -1,6 +1,10 @@
 package ciderblog.cider;
 
+import org.apache.commons.io.FilenameUtils;
 import lombok.Value;
+import org.apache.commons.lang.ObjectUtils;
+
+import java.io.File;
 
 @Value // All fields are private and final. Getters (but not setters) are generated (https://projectlombok.org/features/Value.html)
 public class Cider {
@@ -13,6 +17,24 @@ public class Cider {
     String sniffRating;
 
     public String getCover() {
-        return "/img/" + this.name.replace(" ", "_").toLowerCase() + ".jpg";
+
+        String workDirectory = System.getProperty("user.dir");
+        File folder = new File(workDirectory + "/src/main/resources/public/img");
+        File[] listOfFiles = folder.listFiles();
+        String ciderName = this.name.toLowerCase().replace(" ", "_");
+
+        if (listOfFiles == null)
+            throw new NullPointerException("No files were found in getCover method for the given directory.");
+
+        for(File file : listOfFiles){
+
+            String fileNameWithOutExt = FilenameUtils.removeExtension(file.getName());
+
+            if (file.isFile() && (fileNameWithOutExt.equals(ciderName)))
+                return "/img/" + file.getName();
+        }
+
+        // If we get here, it means that the for loop above did not return an image.
+        return "image not found";
     }
 }
